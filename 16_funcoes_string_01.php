e1e161e46c07faf056311fa0c87a1fb0fbbffa6f
Carlos Eduardo dos Santos Roberto <cadu172@gmail.com>
2017-09-19 14:35:24 -0300

    Continuacao da Aula 16...

--- 16_funcoes_string_01.php
+++ 16_funcoes_string_01.php
@@ -20,23 +20,58 @@
 	<fieldset>
 	<?php
 
-	/*
-	
+	/*	
 	1. printf
 
 		Herdado da linguagem "C", serve para imprimir o resultado na tela
 
-	2. print_r
+	*/
 
+	echo "<p>Função <strong>printf<strong></p>";
+	$valor = 125.3478;
+	$produ = "Sabão";
+	printf("O preço do %s é %.2f",$produ, $valor);
+
+
+	/*
+	2. print_r
 		imprime o conteudo de um array
+
+		Serve para imprimir o conteúdo de um vetor, muito util para DEBUG
+	*/
+	echo "<hr />";
+	echo "<p>Função <strong>print_r<strong></p>";
+	$arrNome = array("Carlos","Duda","Janaina");	
+	print_r($arrNome);
+
+	/*
 
 	3. wordwrap
 
 		inclui uma quebra de linha com base em um caractere especifico
 
-	4. strlen
+		prototipo
 
+		wordwrap($str, tamanho_quebra, caracter_quebra, quebra_fixa)
+
+	*/	
+	echo "<hr />";
+	echo "<p>Função <strong>print_r<strong></p>";
+	$strNome = "CARLOS EDUARDO DOS SANTOS ROBERTO";
+	$strNome = wordwrap($strNome,5,"<br />",false);
+	echo $strNome;
+
+	/*
+	4. strlen
 		contar a quantidade de caracteres de uma string
+	*/
+	echo "<hr />";
+	echo "<p>Função <strong>strlen<strong></p>";
+	$strNome = "CARLOS EDUARDO DOS SANTOS ROBERTO";
+	echo strlen($strNome);
+
+
+	/*
 
 	5. trim
 	
@@ -50,14 +85,28 @@
 
 		remove os espacos em branco a direita de uma string
 
+	*/
+
+	/*
 	8. str_word_count
 
 		conta a quantidade de palavras dentro de uma string
+	*/
+	echo "<hr />";
+	echo "<p>Função <strong>str_word_count<strong></p>";
+	$strNome = "CARLOS EDUARDO DOS SANTOS ROBERTO";
+	echo "quantidade de palavras:  ".str_word_count($strNome);
 
+	/*
 	9. explode
+		converte uma string em array com base em um caracter especificado*/
+	echo "<hr />";
+	echo "<p>Função <strong>explode<strong></p>";
+	$strNome = "CARLOS;EDUARDO;DOS;SANTOS;ROBERTO";
+	echo "A variável strNome foi convertida em: ";
+	print_r(explode(";",$strNome));
 
-		converte uma string em array com base em um caracter especificado
-
+	/*
 	10. str_split
 
 		ocorre o mesmo que a funcao explode
